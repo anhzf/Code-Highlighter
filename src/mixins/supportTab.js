493f5103fe5faf395c1a9e2b4bf2739f1127e258
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2020-10-13 21:37:40 +0700

    feat: add option for indentation style

--- src/mixins/supportTab.js
+++ src/mixins/supportTab.js
@@ -1,4 +1,10 @@
 export default {
+    data() {
+        return {
+            indentStyle: '    ',
+        };
+    },
+
     methods: {
         enableTab(e) {
             const el = e.target;
@@ -6,7 +12,7 @@ export default {
             const carretStart = el.selectionStart;
             const carretEnd = el.selectionEnd;
             // set text area value
-            el.value = `${val.substring(0, carretStart)}\t${val.substring(carretEnd)}`;
+            el.value = `${val.substring(0, carretStart)}${this.indentStyle}${val.substring(carretEnd)}`;
             // set carret position back
             el.selectionStart = el.selectionEnd = carretStart + 1;
             // prevent focus lose
